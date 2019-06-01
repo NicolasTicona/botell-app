e0b2e03546486561e6e4c316df7e6534a4f987c5
Nicolas Ticona Valdivia <nikolasgamertv@gmail.com>
2019-06-01 11:47:44 -0500

    server express

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+
+const mongoose = require('mongoose')
+
+app.use(express.static('public'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// Routes
+app.use(require('./routes.js'))
+
+mongoose.connect('mongodb://localhost:27017/botel-app', {useNewUrlParser: true}, (err, res) => {
+
+    if(err) throw err;
+
+    console.log('Database Online');
+})
+
+
+server.listen(3000, () => {
+    console.log('Server running ...')
+})
