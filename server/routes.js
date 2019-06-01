91b4d49beb99311d6ec316b8915a014902603f06
Nicolas Ticona Valdivia <nikolasgamertv@gmail.com>
2019-06-01 11:52:27 -0500

    server express f

--- server/routes.js
+++ server/routes.js
@@ -1,28 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const Usuario = require('./models/User.model')
+
 router.get('/', (req, res) => {
 
-    let data = req.body
-
-    return new Promise((resolve, reject) => {
-
-        resolve(data)
-
-    })
 
 })
 
-router.get('user', (req, res) => {
-
-    let user = req.body
-
-    return new Promise((resolve, reject) => {
-
-        resolve(data)
-    })
-})
-
-router.post('login')
 
 module.exports = router
