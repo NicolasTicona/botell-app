26a4a7aa912b30b31563a32dc6eaed0b023c10d2
Nicolas Ticona Valdivia <nikolasgamertv@gmail.com>
2019-06-01 13:34:45 -0500

    server

--- server/routes.js
+++ server/routes.js
@@ -3,8 +3,82 @@ const router = express.Router()
 
 const Usuario = require('./models/User.model')
 
-router.get('/', (req, res) => {
+router.get('/users', (req, res) => {
 
+    Usuario.find({})
+        .exec((err, users) => {
+
+            if(err){
+                return res.status(400).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            return res.json({
+                ok: true,
+                users
+            })
+        })
+    
+})
+
+router.post('/login', (req, res) => {
+
+
+    Usuario.findOne({email: data.email}, (err, userDB) => {
+        if(userDB) {
+            res.json({
+                message: 'Esta cuenta ya existe'
+            })
+        }
+        else{
+            let data = req.body
+            let user = new Usuario()
+            user.name = data.name
+            user.email = data.email
+            user.password = data.password
+            user.rank = 0
+        
+            user.save((err, userDB) => {
+                if(err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    })
+                }
+        
+                return res.json({
+                    ok: true,
+                    user: userDB
+                })
+            })
+        
+        }
+        
+    })
+    
+})
+
+router.put('/rank', (req,res) => {
+
+    let data = req.body
+
+    Usuario.findByIdAndUpdate(data.id, {rank: data.rank}, {new: true}, (err, userDB) => {
+
+        if(err) {
+            return res.json({
+                ok: false,
+                err
+            })
+        }
+
+        return res.json({
+            ok: true,
+            user: userDB,
+            data
+        })
+    })        
 
 })
 
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended: false}))
 // Routes
 app.use(require('./routes.js'))
 
-mongoose.connect('mongodb://localhost:27017/botel-app', {useNewUrlParser: true}, (err, res) => {
+mongoose.connect('mongodb://localhost:27017/botell-app', {useNewUrlParser: true}, (err, res) => {
 
     if(err) throw err;
 
